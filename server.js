d99e1256f3642542b7b913e6893060f57d5ead2f
Rags-Mishra <raginimishraanuj@gmail.com>
2025-09-11 23:25:01 +0530

    cors error

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 app.use(cookieParser());
 app.use(cors({
-    origin: "https://attendone.netlify.app/", // frontend URL
+    origin: "https://attendone.netlify.app", // frontend URL
     credentials: true,               // allow cookies/Authorization headers
   }));
 app.use(express.json());
